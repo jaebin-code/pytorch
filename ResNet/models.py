d818b5b25f17d431f4c25eb9d01c1fc283db8879
Jaebin <woqls2323@g.skku.edu>
2024-07-11 00:41:57 +0900

    Implement ViT

--- ResNet/models.py
+++ ResNet/models.py
@@ -58,51 +58,187 @@ class LeNet9(nn.Sequential):
             nn.Linear(84, 10),
         )
 
+
 class BasicBlock(nn.Module):
+
     def __init__(self, in_channel, out_channel, stride=1):
         super(BasicBlock, self).__init__()
-        self.seq = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, 3, stride=stride, padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(out_channel),
-            nn.Conv2d(out_channel, out_channel, 3, stride=1, padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(out_channel),
-        )
+        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+
         self.shortcut = nn.Sequential()
-        if stride != 1:
+        if stride != 1 or in_channel !=  out_channel:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, bias=False),
-                nn.ReLU(),
                 nn.BatchNorm2d(out_channel)
             )
+
     def forward(self, x):
-        return F.relu(self.seq(x) + self.shortcut(x))
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
 
+class Bottleneck(nn.Module):
 
+    def __init__(self, in_channel, out_channel, stride=1):
+        super(Bottleneck, self).__init__()
+        mid_channel = out_channel // 4
+        self.conv1 = nn.Conv2d(in_channel, mid_channel, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channel)
+        self.conv2 = nn.Conv2d(mid_channel, mid_channel, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channel)
+        self.conv3 = nn.Conv2d(mid_channel, out_channel, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
 
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channel != out_channel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channel)
+            )
 
-class ResNet(nn.Sequential):
-    def __init__(self, num_classes=10):
-        super().__init__(
-            nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            BasicBlock(64, 64),
-            BasicBlock(64, 64),
-            BasicBlock(64, 128, stride=2),
-            BasicBlock(128, 128),
-            BasicBlock(128, 128),
-            BasicBlock(128, 256, stride=2),
-            BasicBlock(256, 256),
-            BasicBlock(256, 256),
-            BasicBlock(256, 512, stride=2),
-            BasicBlock(512, 512),
-            BasicBlock(512, 512),
-            nn.AvgPool2d(4),
-            nn.Flatten(),
-            nn.Linear(512, num_classes),
-        )
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channel = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, out_channel, blocks, stride=1):
+        strides = [stride] + [1] * (blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channel, out_channel, stride))
+            self.in_channel = out_channel
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = self.flatten(x)
+        x = self.fc(x)
+
+        return x
+
+class ResNeXtBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, cardinality, bottleneck_width, stride=1):
+        super(ResNeXtBlock, self).__init__()
+        D = cardinality * bottleneck_width
+        self.conv1 = nn.Conv2d(in_channels, D, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(D)
+        self.conv2 = nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(D)
+        self.conv3 = nn.Conv2d(D, out_channels, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += self.downsample(x)
+        out = self.relu(out)
+
+        return out
+
+class ResNeXt(nn.Module):
+    def __init__(self, block, layers, cardinality, bottleneck_width, num_classes=10):
+        super(ResNeXt, self).__init__()
+        self.cardinality = cardinality
+        self.bottleneck_width = bottleneck_width
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, self.cardinality, self.bottleneck_width, stride))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels, self.cardinality, self.bottleneck_width))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def resnext():
+    return ResNeXt(ResNeXtBlock, [2, 2, 2, 2], cardinality=4, bottleneck_width=16)
+
+def ResNet18_Basic(num_classes=10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
+
+def ResNet18_Bottle(num_classes=10):
+    return ResNet(Bottleneck, [2, 2, 2, 2], num_classes)
 
 def get_model(name):
     if name == "LeNet5":
@@ -111,7 +247,11 @@ def get_model(name):
         return LeNet7()
     elif name == "LeNet9":
         return LeNet9()
-    elif name == "ResNet":
-        return ResNet()
+    elif name == "ResNet18_Basic":
+        return ResNet18_Basic()
+    elif name == "ResNet18_Bottle":
+        return ResNet18_Bottle()
+    elif name == "resnext":
+        return resnext()
     else:
         raise ValueError("Unsupported model")
--- ViT/main.py
+++ ViT/main.py
@@ -1,28 +1,47 @@
-import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 import os
+
 from data import get_dataset
 from models import get_model
 from utils import setup_logger
 
-
 def main():
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--dataset', type=str, required=True, choices=['CIFAR10', 'CIFAR100', 'SVHN'])
-    parser.add_argument('--model', type=str, required=True, choices=['LeNet5', 'LeNet7', 'LeNet9', 'ResNet18_Basic', 'ResNet18_Bottle'])
-    parser.add_argument('--epochs', type=int, default=32)
+    parser.add_argument('--model', type=str, default='ViT')
+    parser.add_argument('--img_size', type=int, default=32)
+    parser.add_argument('--patch_size', type=int, default=4)
+    parser.add_argument('--in_channels', type=int, default=3)
+    parser.add_argument('--num_classes', type=int, default=10)
+    parser.add_argument('--hidden_size', type=int, default=384)
+    parser.add_argument('--num_heads', type=int, default=6)
+    parser.add_argument('--mlp_dim', type=int, default=3072)
+    parser.add_argument('--depth', type=int, default=8)
+
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--lr', type=float, default=0.1)
-    parser.add_argument('--momentum', type=float, default=0.9)
-    parser.add_argument('--wd', type=float, default=0.0001)
-    parser.add_argument('--logdir', type=str, default='logs')
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--logdir', type=str, default='./logs')
+
 
     args = parser.parse_args()
 
-    trainloader, testloader = get_dataset(args.dataset, args.batch_size)
-    model = get_model(args.model)
+    train_loader, test_loader = get_dataset(args.dataset, args.batch_size)
+    model = get_model(
+        args.model,
+        img_size=args.img_size,
+        patch_size=args.patch_size,
+        in_channels=args.in_channels,
+        num_classes=args.num_classes,
+        hidden_size=args.hidden_size,
+        num_heads=args.num_heads,
+        mlp_dim=args.mlp_dim,
+        depth=args.depth,
+    )
 
     logdir = os.path.join(args.logdir, f'{args.model}_{args.dataset}')
     logger, writer = setup_logger(logdir)
@@ -30,12 +49,13 @@ def main():
 
     model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.wd)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(args.epochs * 0.5), int(args.epochs * 0.75)], gamma=0.1)
 
     for epoch in range(args.epochs):
+        model.train()
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(train_loader, 0):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
@@ -45,12 +65,13 @@ def main():
             optimizer.step()
             running_loss += loss.item()
             logger.info(f'[{epoch + 1}, {i + 1}] loss: {loss.item():.3f}')
-            writer.add_scalar('training loss', loss.item(), epoch * len(trainloader) + i)
+            writer.add_scalar('training loss', loss.item(), epoch * len(train_loader) + i)
 
+        model.eval()
         correct = 0
         total = 0
         with torch.no_grad():
-            for data in testloader:
+            for data in test_loader:
                 images, labels = data
                 images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
